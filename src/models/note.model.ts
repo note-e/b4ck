ca0ca50a9f446daaceb6606d0a596d4fd351f5de
kerollos <kerolloz@yahoo.com>
2019-10-09 02:32:51 +0200

    add note model

---
+++ src/models/note.model.ts
@@ -0,0 +1,62 @@
+import _ from 'lodash';
+import mongoose from 'mongoose';
+
+import joi from '@hapi/joi';
+
+// import {enumToArray} from '../utils';
+
+export interface INote {
+  title: string;
+  description: string;
+  user: object;
+  // status: string;
+  // deadline: Date;
+}
+
+export interface INoteModel extends INote, mongoose.Document {}
+
+export enum NoteStatus {
+  TODO,
+  DONE,
+}
+
+export const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    // status: {
+    //   type: String,
+    //   enum: enumToArray(NoteStatus),
+    //   default: NoteStatus[NoteStatus.TODO],
+    // },
+    // deadline: {
+    //   type: Date,
+    // },
+  },
+  {timestamps: true},
+);
+
+export const Note = mongoose.model<INoteModel>('Note', noteSchema);
+export const canEdit = ['title', 'description'];
+
+export function validateNote(note: any) {
+  const schema = joi.object({
+    title: joi.string().required(),
+    description: joi.string().required(),
+    user: joi.any().required(),
+    // status: joi.string().allow(enumToArray(NoteStatus)),
+    // deadline: joi.date(),
+  });
+  return schema.validate(note);
+}
