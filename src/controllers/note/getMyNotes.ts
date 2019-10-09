20513f81a08f30a07e9bf69d7dac8e008fbfb6e7
kerollos <kerolloz@yahoo.com>
2019-10-09 02:25:18 +0200

    implement get my notes

---
+++ src/controllers/note/getMyNotes.ts
@@ -0,0 +1,13 @@
+import {Request, Response} from 'express';
+
+import {IAuthenticatedRequest} from '../../middlewares';
+import {Note} from '../../models/note.model';
+import APIResponse from '../../utils/APIResponse';
+
+export async function getMyNotes(req: Request, res: Response) {
+  const user = (req as IAuthenticatedRequest).authenticatedUser.userID;
+  const notes = await Note.find({
+    user,
+  });
+  return APIResponse.Ok(res, notes);
+}
