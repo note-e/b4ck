9a22bab89d4badf84eb129ce47054c9bd685ace5
kerollos <kerolloz@yahoo.com>
2019-10-09 02:25:27 +0200

    implement update note

---
+++ src/controllers/note/updateNote.ts
@@ -0,0 +1,26 @@
+import {Request, Response} from 'express';
+import _ from 'lodash';
+
+import {IAuthenticatedRequest} from '../../middlewares';
+import {Note} from '../../models/note.model';
+import APIResponse from '../../utils/APIResponse';
+
+export async function updateNote(req: Request, res: Response) {
+  const user = (req as IAuthenticatedRequest).authenticatedUser;
+  const _id = req.params.id; // note id
+  const note = await Note.findById(_id);
+
+  if (!note) return APIResponse.NotFound(res, `No note with id: ${_id}`);
+  if (note.user.toString() !== user.userID)
+    return APIResponse.Forbidden(res, 'Access Denied!');
+
+  _.extend(note, req.body);
+
+  try {
+    await note.save();
+    return APIResponse.Ok(res, note);
+  } catch (err) {
+    console.error(err);
+    return APIResponse.UnprocessableEntity(res, err.message);
+  }
+}
