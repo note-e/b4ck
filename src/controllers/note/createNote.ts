4a4c82b13505a91fb361be2758a6adac3c427229
kerollos <kerolloz@yahoo.com>
2019-10-09 02:25:11 +0200

    implement create note

---
+++ src/controllers/note/createNote.ts
@@ -0,0 +1,14 @@
+import {Request, Response} from 'express';
+
+import {IAuthenticatedRequest} from '../../middlewares';
+import {Note, validateNote} from '../../models/note.model';
+import APIResponse from '../../utils/APIResponse';
+
+export async function createNote(req: Request, res: Response) {
+  req.body.user = (req as IAuthenticatedRequest).authenticatedUser.userID;
+  const {error} = validateNote(req.body);
+  if (error) return APIResponse.UnprocessableEntity(res, error.message);
+  const note = new Note(req.body);
+  await note.save();
+  return APIResponse.Ok(res, note);
+}
